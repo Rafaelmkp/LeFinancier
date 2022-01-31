c04d4902b9c82bae754f7876fc5ac6d0b8c5b03e
Rafaelmkp <rafaelmobarack@gmail.com>
2022-01-31 20:48:28 -0300

    feat(index): import index.

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import userRouter from './routes/user/user-routes.js';
+import userRouter from './routes/user/userRoutes.js';
 
 const app = express();
 app.use(express.json());
