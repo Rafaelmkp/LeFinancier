fd1cbefdf90ef23f7451028612d64641b5376b12
Rafaelmkp <rafaelmobarack@gmail.com>
2022-02-01 08:56:35 -0300

    refactor(backend): corrigir estrutura projeto.

---
+++ backend/src/dao/user/IUserDao.js
@@ -0,0 +1,17 @@
+export default class IUserDao {
+  createUser(user) {
+    console.log("UserDao - method not implemented yet!");
+  }
+
+  readUser(id) {
+    console.log("UserDao - method not implemented yet!");
+  }
+
+  updateUser(user) {
+    console.log("UserDao - method not implemented yet!");
+  }
+
+  deleteUser(id) {
+    console.log("UserDao - method not implemented yet!");
+  }
+}
---
+++ backend/src/dao/user/UserConstants.js
@@ -0,0 +1,4 @@
+export default Object.freeze({
+  USER_FILE_PATH: '',
+  USER_FILE_NAME: ''
+});
