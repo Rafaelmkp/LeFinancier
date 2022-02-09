3a52fd3022e7993c89336a22c5e2f4ff74fe0384
Rafaelmkp <rafaelmobarack@gmail.com>
2022-02-09 18:33:56 -0300

    feat(userRoutes) : create endpoint for user creation.

--- backend/src/routes/user/userRoutes.ts
+++ backend/src/routes/user/userRoutes.ts
@@ -8,15 +8,21 @@ const userSerive = new UserService(userDao);
 const router = express.Router();
 
 router.post('/new-user', async (req, res, next) => {
-  //do persist new user
+  try{ 
+    const user = req.body;
+    const userCreated = await userSerive.createUser(user);
+    res.status(200).send(userCreated);
+  } catch(err) {
+    next(new Error('not possible to create the user'));
+  }
 });
 
 
 router.get('/users', async (req, res, next) => {
   try{
-    const users = userSerive.getAllUsers();
+    const users = await userSerive.getAllUsers();
     res.status(200).send(users);
-  } catch (err) {
+  } catch(err) {
     console.log(err);
     next(new Error('not possible to retrieve users'));
   }
