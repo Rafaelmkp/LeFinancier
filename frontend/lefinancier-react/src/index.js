995253d04b8759f5d28812066267ce6a64e47959
Rafaelmkp <rafaelmobarack@gmail.com>
2022-02-12 11:49:18 -0300

    feat(frontend): update index.js.

--- frontend/lefinancier-react/src/index.js
+++ frontend/lefinancier-react/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Routes from './Routes';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import './Style/App.css';
+import './Style/grid.css';
+import './Style/input.css';
+
+
+ReactDOM.render( <Routes />, document.getElementById('root') );
