a72200a3741b2d24d7a13ee477ce6eddac3742bd
Rafaelmkp <rafaelmobarack@gmail.com>
2022-02-12 11:41:54 -0300

    feat(frontend): add routes.

---
+++ frontend/lefinancier-react/src/Routes.js
@@ -0,0 +1,11 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+function Routes() {
+  return (
+    <Router forceRefresh>
+      <Route path='/sign' exact component={} />
+    </Router>
+  );
+}
+
+export default App;
