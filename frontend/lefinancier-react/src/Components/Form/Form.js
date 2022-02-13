ed8626fc0d69f58851dc53e13b0e335a79f736fd
Rafaelmkp <rafaelmobarack@gmail.com>
2022-02-13 12:06:19 -0300

    feat(Form): add onsubmit property.

--- frontend/lefinancier-react/src/Components/Form/Form.js
+++ frontend/lefinancier-react/src/Components/Form/Form.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export default function Form({children, className}) {
+export default function Form({children, className, onSubmit}) {
   return (
-    <form className={className}>
+    <form className={className} onSubmit={onSubmit}>
       {children}
     </form>
   )
