cb1984a284c792f71db6c1c0e159971f5b99ec70
Rafaelmkp <rafaelmobarack@gmail.com>
2022-02-23 22:02:23 -0300

    feat(PageSignUp): add page component for sign up.

---
+++ frontend/lefinancier-react/src/Pages/PageSignUp.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import FormSignUp from '../Components/Form/FormSignUp';
+import Page from './Page';
+
+import '../Style/signup.css';
+
+export default function PageSignUp() {
+  const submitForm = () => {
+    console.log("signed up!!");
+  }
+
+  return (
+    <Page>
+      <div className='row valign-wrapper' >
+        <div className='col m6 l6 xl6'>
+          <div className='signup-side-info light-blue-4' >
+            <span>This is a sign up form</span>
+          </div>
+        </div>
+        <div className='col s12 m6 l6 xl6 light-blue-2 signup-form'>
+          <div className='row'>
+            <div className='col m2 l2 xl2' ></div>
+            <div className='col s12 m10 l10 xl10'>
+              <div className='row'>
+                <FormSignUp onSubmit={submitForm}/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Page>
+  )
+}
