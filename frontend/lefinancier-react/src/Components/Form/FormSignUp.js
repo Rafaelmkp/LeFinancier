a686423a89d408852cddefa0630fc27c38063a0b
Rafaelmkp <rafaelmobarack@gmail.com>
2022-02-23 21:59:40 -0300

    feat(FormSignUp): add form component for sign up.

--- frontend/lefinancier-react/src/Components/Form/FormSignUp.js
+++ frontend/lefinancier-react/src/Components/Form/FormSignUp.js
@@ -1,16 +1,36 @@
 import React from 'react'
+import InputEmail from '../Input/InputEmail';
+import InputPhone from '../Input/InputPhone';
 import InputText from '../Input/InputText';
 import Form from './Form';
 
-export default function FormSignUp() {
+export default function FormSignUp({onSubmit}) {
+  const handleSubmit = (evt) => {
+    onSubmit && onSubmit();
+    evt.preventDefault();
+  }
+
   return (
-    <Form className='col s10 m6 l6 xl6' >
+    <Form className='col s12 m12 l12 xl12' onSubmit={(evt) => handleSubmit(evt)}>
       <InputText
-        labael='Username'
+        label='Username'
         name='username'
         placeholder='Insert your username'
         required={true}
       />
+      <InputEmail 
+        label='E-mail'
+        name='email'
+        placeholder='Insert your e-mail'
+        required={true}
+      />
+      <InputPhone 
+        label='Phone'
+        name='phone'
+        placeholder='Insert your phone number'
+        required={true}
+      />
+      <input type="submit"/>
     </Form>
   )
 }
