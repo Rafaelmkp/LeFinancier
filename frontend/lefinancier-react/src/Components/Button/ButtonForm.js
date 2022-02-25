30b4766e23a59b7ff552f4c0ef8f87d7b7ad3da8
Rafaelmkp <rafaelmobarack@gmail.com>
2022-02-25 13:28:08 -0300

    feat(ButtonForm): add css class for text color.

--- frontend/lefinancier-react/src/Components/Button/ButtonForm.js
+++ frontend/lefinancier-react/src/Components/Button/ButtonForm.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function ButtonInput({type, text}) {
   return (
-    <input  className='button-form' type={type} value={text}/>
+    <input  className='button-form txt-white-1' type={type} value={text.toUpperCase()}/>
   )
 }
