257c6c606ba14d71ca41ea08db64da18f86e83d0
Rafaelmkp <rafaelmobarack@gmail.com>
2022-02-27 16:29:10 -0300

    refactor(ButtonForm): change button form style to support reset input.

--- frontend/lefinancier-react/src/Components/Button/ButtonForm.js
+++ frontend/lefinancier-react/src/Components/Button/ButtonForm.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function ButtonInput({type, text}) {
   return (
-    <input  className='button-form txt-white-1' type={type} value={text.toUpperCase()}/>
+    <input  className={`button-form ${type}`} type={type} value={text.toUpperCase()}/>
   )
 }
