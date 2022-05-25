49034f7301fca866c7305a8e69cb23b608f4fcf5
Rafaelmkp <rafaelmobarack@gmail.com>
2022-05-24 22:04:30 -0300

    feat(backend-java): add java backend project skeleton.

---
+++ backend-java/LeFinancier/src/main/java/org/rafaelmkp/LeFinancierApplication.java
@@ -0,0 +1,11 @@
+package org.rafaelmkp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeFinancierApplication {
+    public static void main( String[] args ) {
+        SpringApplication.run(LeFinancierApplication.class, args );
+    }
+}
